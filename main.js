fcdfd36a43cc269ff11c93dc8da9ecad167b2793
Paul Shivers <paulsshivers@gmail.com>
2020-04-11 16:08:08 -0400

    stopping for a bit

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
-$(function () {
-  for (let j = 0; j < 5; j++) {
-    const row = $('<div/>', {
-      id: 'row-' + j,
-      class: 'row',
-    });
+const cards = ['heart', 'heart', 'diamond', 'diamond', 'club', 'club'];
+let randomNumber = Math.floor(Math.random() * 10);
 
-    $('#game-board').append(row);
-    for (let i = 0; i < 4; i++) {
-      const card = $('<div/>', {
-        id: `${j}${i}`,
-        class: 'card',
-        title: 'card ' + i,
-        text: 'cardhidden',
-        properties: { isShown: false },
-        on: {
-          click: function (e) {
-            flipCard(e.target.id);
-          },
-        },
-      });
-      $(row).append(card);
-    }
+assignCards = function (i) {
+  while (i < cards.length) {
+    console.log(Math.floor(Math.random() * 10));
+    i++;
   }
+};
+
+assignCards(0);
+
+$(function () {
+  initializeBoard = function () {
+    for (let j = 0; j < 2; j++) {
+      const row = $('<div/>', {
+        id: 'row-' + j,
+        class: 'row',
+      });
+
+      $('#game-board').append(row);
+      for (let i = 0; i < 3; i++) {
+        const card = $('<div/>', {
+          id: `${j}${i}`,
+          class: 'card',
+          title: 'card ' + i,
+          text: 'cardhidden',
+          on: {
+            click: function (e) {
+              flipCard(e.target.id);
+            },
+          },
+        });
+        $(row).append(card);
+      }
+    }
+  };
+
+  initializeBoard();
 
   flipCard = function (id) {
     const $card = $('#' + id);
